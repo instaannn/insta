6785d2a3476342ccc102f9860429323a9b7ce858
Anna Sycheva <anna.sycheva@cleverpumpkin.ru>
2022-10-17 11:54:00 +0300

    initial commit

---
+++ Insta/Insta/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Insta
+//
+//  Created by Анна Сычева on 17.10.2022.
+//
+
+import UIKit
+
+/// AppDelegate
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+}
---
+++ Insta/Insta/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  Insta
+//
+//  Created by Анна Сычева on 17.10.2022.
+//
+
+import UIKit
+
+/// SceneDelegate
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) { }
+}
